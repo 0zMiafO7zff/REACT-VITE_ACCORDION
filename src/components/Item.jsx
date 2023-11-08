7bb4212910c37906e43e235c494ee55dcbbd3316
kittipan <kittipan.mtz.062@gmail.com>
2023-11-08 11:22:32 +0700

    FIRST

---
+++ src/components/Item.jsx
@@ -0,0 +1,15 @@
+import "./Item.css"
+import { useState } from "react";
+export default function Item(props) {
+    const {id,title,description} = props
+    const [show,setShow] = useState(false)
+  return (
+    <section>
+      <div className="content">
+        <h4>{title}</h4>
+        <button onClick={()=>setShow(!show)}>{show ? "Hide" : "Show"}</button>
+      </div>
+      {show && <p>{description}</p>}
+    </section>
+  );
+}
